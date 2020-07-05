c55cfaba2aba7b61bfb3f5664746f27182c26cd4
Ajay Pathak <APathak2@slb.com>
2020-07-05 06:29:12 +0530

    Added Error

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -24,6 +24,14 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
             app.UseRouting();
             app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
             app.Run(async (context) =>
